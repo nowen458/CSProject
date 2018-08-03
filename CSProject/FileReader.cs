ac246bc76a78d00367f3e86a5f053b9044d1fc70
Nate Owen <nateowen@me.com>
2018-08-03 16:11:40 -0400

    Added FileReader class

--- CSProject/FileReader.cs
+++ CSProject/FileReader.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace CSProject
 {
     public class FileReader
@@ -6,5 +9,15 @@ namespace CSProject
         public FileReader()
         {
         }
+
+        public List<Staff> ReadFile() {
+            List<Staff> myStaff = new List<Staff>();
+            string[] result = new string[2];
+            string path = "staff.rtf";
+            string[] separator = { ", " };
+        }
+
+        if(File.exists(path)) {
+            
     }
 }
