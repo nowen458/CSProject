da4a8b36b5949d0f45a758f5e91ad46f5221d2e9
Nate Owen <nowen747@gmail.com>
2018-08-27 20:47:16 -0400

    Created PaySlip class

---
+++ CSProject/PaySlip.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSProject
+{
+    class PaySlip
+    {
+        // declaring fields
+        private int month;
+        private int year;
+
+        // declaring enum
+        enum MonthsOfYear
+        {
+            JAN = 1,
+            FEB = 2,
+            MAR = 3,
+            APR = 4,
+            MAY = 5,
+            JUN = 6,
+            JUL = 7,
+            AUG = 8,
+            SEP = 9,
+            OCT = 10,
+            NOV = 11,
+            DEC = 12,
+        }
+
+        // PaySlip Constructor
+        public PaySlip (int payMonth, int payYear)
+        {
+            month = payMonth;
+            year = payYear;
+        }
+    }
+}
