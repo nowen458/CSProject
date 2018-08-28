6de3f23678b49aec670c2bf561d3941215f8c00d
Nate Owen <nowen747@gmail.com>
2018-08-27 21:47:31 -0400

    Started working on the Main method in Program to accept input from user

--- CSProject/Program.cs
+++ CSProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSProject
 {
@@ -6,7 +7,49 @@ namespace CSProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Staff> myStaff = new List<Staff>();
+            FileReader fr = new FileReader();
+            int month = 0;
+            int year = 0;
+
+            while (year == 0)
+            {
+                Console.WriteLine("\nPlease enter year: ");
+
+                try
+                {
+                    // Code to convert the input to an interger
+                    year = Convert.ToInt32(Console.ReadLine());
+                } // end try
+                catch (FormatException e)
+                {
+                    // Code to handle the exception
+                    Console.WriteLine("Unfortunately, there was an error converting the year to an interger: " + e.Message);
+                } // end catch
+            } // end year while
+
+            while (month == 0)
+            {
+                Console.WriteLine("\nPlease enter month: ");
+
+                try
+                {
+                    month = Convert.ToInt32(Console.ReadLine());
+                    if ((month > 0) && (month < 13))
+                    {
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Month needs to be between 1 and 12. Please try again.");
+                        month = 0;
+                    }
+                } // end try
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Unfortunately, there was an error converting the month to an interger: " + e.Message);
+                } // end catch
+            } // end month while
         }
     }
 }
